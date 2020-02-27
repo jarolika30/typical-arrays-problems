a39f5701487721964149cfc47aa6d9bef7ca902d
jarolika30 <44402789+jarolika30@users.noreply.github.com>
2020-02-27 13:14:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (typeof array === 'undefined' || array.length === 0) ? 0 : array.reduce((cur, prev) => Math.min(cur, prev));
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (typeof array === 'undefined' || array.length === 0) ? 0 : array.reduce((cur, prev) => Math.max(cur, prev));
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (typeof array === 'undefined' || array.length === 0) ? 0 : (array.reduce((cur, prev) => cur + prev) / array.length);
 }
